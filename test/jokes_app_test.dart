dee810b6506edd7326945f389fe64ad01b1aa696
Roger Bacab <roger.bacab7@gmail.com>
2024-08-29 13:59:57 -0600

    :white_check_mark: adding app tests

---
+++ test/jokes_app_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jokes_app/home_page.dart';
+import 'package:jokes_app/jokes_app.dart';
+import 'package:jokes_repository/jokes_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockJokesRepository extends Mock implements JokesRepository {}
+
+void main() {
+  late JokesRepository jokesRepository;
+
+  setUp(() {
+    jokesRepository = MockJokesRepository();
+  });
+
+  group('Jokes App', () {
+    testWidgets('Renders MaterialApp with dark theme', (tester) async {
+      await tester.pumpWidget(JokesApp(jokesRepository: jokesRepository));
+
+      expect(find.byType(MaterialApp), findsOneWidget);
+
+      final materialApp = tester.widget<MaterialApp>(find.byType(MaterialApp));
+      expect(materialApp.theme, equals(ThemeData.dark()));
+    });
+
+    testWidgets('Renders Home Page', (tester) async {
+      await tester.pumpWidget(JokesApp(jokesRepository: jokesRepository));
+
+      expect(find.byType(HomePage), findsOneWidget);
+    });
+  });
+}
